3a728c744cca5eeb4030c70770b1c87c6d100b21
= <=>
2023-07-05 12:53:48 -0400

    fix: use only s3 for terms source

--- src/components/Terms/Terms.vue
+++ src/components/Terms/Terms.vue
@@ -42,10 +42,10 @@ export default {
 
 			if (response.status == 200) {
 				let that        = this;
-				this.pdf_name   = response.detail.name;
-				this.pdf_ext    = response.detail.ext;
-				this.pdf_source = `${this.$root.api_url}/documents/terms-of-service.${this.pdf_ext}?v=${Math.random().toString().split('.')[1]}`;
-				// this.pdf_source = response.detail.url;
+				this.pdf_name   = response.detail?.name;
+				this.pdf_ext    = response.detail?.ext;
+				// this.pdf_source = `${this.$root.api_url}/documents/terms-of-service.${this.pdf_ext}?v=${Math.random().toString().split('.')[1]}`;
+				this.pdf_source = response.detail?.url;
 
 				if (this.pdf_ext == 'txt') {
 					fetch(this.pdf_source).then(function(resp) {
